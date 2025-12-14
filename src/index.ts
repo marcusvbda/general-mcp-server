bde5a3cfa3560ea4d6b95487e1d60410546e1e84
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-12-14 13:14:51 -0300

    concluido

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,5 @@ mcpServer.setTools({
 });
 
 orchestrator.setAssistantDescription("Você é um assistente de IA que responde perguntas sobre o usuario. Seu nome é TARS - se for pedido JSON, responda APENAS o JSON, nada além disso.");
-const response = await orchestrator.ask("Qual é seu nome, o nome do usuario e quem foi o campeao da formula 1 em 2025?");
+const response = await orchestrator.ask("Diga olá pro usuario");
 console.log(response);
